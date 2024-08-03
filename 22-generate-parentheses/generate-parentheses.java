ffeae0b98db5485a9a7fb0fa09da9012bb6cb07e
Viswa_2004 <154723533+vishperiyasamy@users.noreply.github.com>
2024-08-03 17:45:16 +0530

    Time: 1 ms (85.64%) | Memory: 43.2 MB (48.03%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result=new ArrayList<>();
+        String str="";
+        solve(n,str,result,0,0);
+         return result;
+    }
+   public void  solve(int n,String str,List<String> result,int open_count,int close_count){
+        if(open_count==n && close_count==n){
+            result.add(str);
+            return;
+        }
+        else{
+            if(open_count < n){
+                solve(n,str+"(",result,open_count+1,close_count);
+            }
+            if(close_count<open_count){
+               solve(n,str+")",result,open_count,close_count+1); 
+            }
+        }
+    }
+}
