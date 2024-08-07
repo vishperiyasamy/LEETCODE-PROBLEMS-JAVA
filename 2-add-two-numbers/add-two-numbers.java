51b4967735d21dab87a875b669e025ebf42492c7
Viswa_2004 <154723533+vishperiyasamy@users.noreply.github.com>
2024-08-07 21:38:02 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (35.00%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,54 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode first = null;
-        ListNode currentNode = null;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        ListNode dummy = new ListNode(-1); 
+        ListNode curr = dummy;  
         int carry = 0;
-        
-        while (l1 != null && l2 != null) {
-            int sum = l1.val + l2.val + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-            ListNode value = new ListNode(digit);
-            if (first == null) {
-                first = value;
-            }
-            if (currentNode == null) {
-                currentNode = value;
-            } else {
-                currentNode.next = value;
-                currentNode = value;
-            }
-            l1 = l1.next;
+        while(l1!=null || l2!=null)
+        {
+            int digit1=0,digit2=0;
+            if(l1!=null)
+            digit1 = l1.val;  //
+            if(l2!=null)
+            digit2 = l2.val;  
+            int rem=(digit1+digit2+carry)%10; 
+            ListNode temp = new ListNode(rem); 
+            carry = (digit1+digit2+carry)/10; 
+            curr.next = temp;  
+            curr = curr.next;
+            if(l1!=null)
+            l1 = l1.next; 
+            if(l2!=null)
             l2 = l2.next;
+        }  
+        if(carry==1) 
+        {
+            curr.next = new ListNode(carry);
         }
-        
-        ListNode remaining = (l1 == null) ? l2 : l1;
-        while (remaining != null) {
-            int sum = remaining.val + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-            ListNode value = new ListNode(digit);
-            currentNode.next = value;
-            currentNode = value;
-            remaining = remaining.next;
-        }
-        
-        if (carry != 0) {
-            ListNode value = new ListNode(carry);
-            currentNode.next = value;
-            currentNode = value;
-        }
-        
-        return first;
-    }
+        return dummy.next;
+    } 
 }
