1ce8ab301c783ec2252a2fe709269e87bbfcd96d
Viswa_2004 <154723533+vishperiyasamy@users.noreply.github.com>
2024-08-17 16:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (47.02%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public void solve(int[] nums, List<List<Integer>> result, int start) {
+        if (start == nums.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int num : nums) {
+                list.add(num);
+            }
+            result.add(list);
+            return;
+        }
+        
+        for (int i = start; i < nums.length; i++) {
+            // Swap the current index with the start index
+            swap(nums, start, i);
+            // Recur for the next index
+            solve(nums, result, start + 1);
+            // Backtrack by swapping back
+            swap(nums, start, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        solve(nums, result, 0);
+        return result;
+    }
+}
