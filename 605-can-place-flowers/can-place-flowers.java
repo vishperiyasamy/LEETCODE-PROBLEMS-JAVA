57e1c2316fd03e359449c4c293b2c0601ee0939b
Viswa_2004 <154723533+vishperiyasamy@users.noreply.github.com>
2024-08-20 16:01:34 +0530

    Time: 1 ms (97.57%) | Memory: 45.6 MB (27.24%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,29 +1,38 @@
-class Solution {
-    public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int count = 0;
-        int length = flowerbed.length;
-        
-        // Use a single loop to check each plot
-        for (int i = 0; i < length; i++) {
-            // Check if the current plot is empty
-            if (flowerbed[i] == 0) {
-                // Check if the previous and next plots are also empty
-                boolean prevEmpty = (i == 0) || (flowerbed[i - 1] == 0);
-                boolean nextEmpty = (i == length - 1) || (flowerbed[i + 1] == 0);
-                
-                if (prevEmpty && nextEmpty) {
-                    // Plant a flower here
-                    flowerbed[i] = 1;
-                    count++;
-                    
-                    // Move to the next position where we can start checking
-                    // Skipping the next plot since it cannot have a flower
-                    i++;
+class Solution 
+{
+    public boolean canPlaceFlowers(int[] f, int n) 
+    {
+        for(int i=0;i<f.length;i++)
+        {
+            if(i-1>=0&&i+1<f.length)
+            {
+                if(f[i-1]==0 && f[i]==0 && f[i+1]==0)
+                {
+                    f[i]=1;
+                    n--;
                 }
             }
+            else if(i-1>=0)
+            {
+                 if(f[i-1]==0 && f[i]==0)
+                {
+                    f[i]=1;
+                    n--;
+                }
+            }
+            else if(i+1<f.length)
+            {
+                 if(f[i+1]==0 && f[i]==0)
+                {
+                    f[i]=1;
+                    n--;
+                }
+            }
+            else
+            {
+                if(f[i]!=1)n--;
+            }
         }
-        
-        // Check if we have managed to plant at least n flowers
-        return count >= n;
+        return n<=0;
     }
 }
