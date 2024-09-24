14c9afd55d38d9574f2c2cff950d3a66367da681
Viswa_2004 <154723533+vishperiyasamy@users.noreply.github.com>
2024-09-24 14:32:44 +0530

    Time: 44 ms (39.64%) | Memory: 44.6 MB (88.17%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,38 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int res[]=new int[2];
-        int []n=new int[nums.length];
-        int n1=0;
-        int n2=0;
-        for(int k=0;k<nums.length;k++)
-        {
-            n[k]=nums[k];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j}; 
+                }
+            }
         }
-        Arrays.sort(nums);
-        int i=0;
-        int j=nums.length-1;
-        while(i<j)
-        {
-            int sum=nums[i]+nums[j];
-            if(sum<target){i++;}
-            else if(sum>target){j--;}
-            else {n1=nums[i];n2=nums[j];break;}
-        }
-        int x=0;
-      
-        
-         for(int k=0;k<nums.length;k++)
-        {
-            if(n[k]==n1&&x==0) {res[0]=k;x++;}
-             if(n[k]==n2){ res[1]=k;}
-        }
-
-        return res;
-       
-    
+        return new int[] {}; 
     }
 }
-
-
-     
-    
